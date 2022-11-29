23e6b6da0491157e6a123738522b635026d1bbe5
Bryan Makini <bmakini@ymail.com>
2022-11-29 16:02:33 +0000

    Created and configured the conectionstring in the appsettings.json and program.cs respetiveley.

--- EmployeeAPI/Program.cs
+++ EmployeeAPI/Program.cs
@@ -1,11 +1,18 @@
+using EmployeeAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var config = builder.Configuration.GetConnectionString("DefaultConnectionString");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<EmployeeDbContext>(options =>
+{
+    options.UseSqlServer(config);
+});
 
 var app = builder.Build();
 
