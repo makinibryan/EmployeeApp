a2c9864a1307e95280c51907075b8dfe24f7ec57
Bryan Makini <bmakini@ymail.com>
2022-11-29 16:38:32 +0000

    Created the get and post methods on the EmployeesController

---
+++ EmployeeAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeAPI.Data;
+using EmployeeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeDbContext _context;
+
+        public EmployeesController(EmployeeDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees() 
+        {
+            var employees = await _context.Employees.ToListAsync();
+
+            return Ok(employees) ;
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee([FromBody] Employee employee)
+        {
+           employee.Id= Guid.NewGuid();    
+
+           await _context.Employees.AddAsync(employee);
+           await _context.SaveChangesAsync();
+           return Ok(employee);
+        }
+       
+    }
+}
