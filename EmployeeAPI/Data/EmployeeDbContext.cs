a331392c49cbf38ab301e79f8cd8b4617b03fe13
Bryan Makini <bmakini@ymail.com>
2022-11-29 15:55:26 +0000

    Created the DbContext

---
+++ EmployeeAPI/Data/EmployeeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAPI.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
